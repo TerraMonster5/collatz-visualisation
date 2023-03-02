2564e2ea98f7e539f7ff8d3dc40560a4dce41173
TerraMonster5 <93338246+TerraMonster5@users.noreply.github.com>
2023-03-02 10:56:18 +0000

    Added graph drawing

--- src/visualiser.js
+++ src/visualiser.js
@@ -1,4 +1,27 @@
 window.onload = function () {
-    var start = new URLSearchParams(window.location.search).get("start")
-    console.log(start)
+    var start = new URLSearchParams(window.location.search).get("start"),
+    current = Number(start),
+    canvas = document.getElementById("graph"),
+    canvasHeight = Number(canvas.style.height),
+    ctx = canvas.getContext("2d"),
+    i = 1;
+
+    ctx.moveTo(canvasHeight,current);
+
+    while (current != 1) {
+        ctx.lineTo(canvasHeight-i, current);
+        ctx.stroke();
+
+        if (current % 2) {
+            current *= 3;
+            current += 1;
+        }
+        else {
+            current = Number(current / 2);
+        }
+
+        console.log(current);
+
+        i++;
+    }
 }
